9641eb01699706ea0832d3ad7373b9d4551e8ba6
Elendu Ifeanyi <goodchild66@gmail.com>
2022-05-20 06:00:36 +0100

    added updateClicked method

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import he from 'he';
-import { nanoid } from 'nanoid';
+import React from "react";
+import he from "he";
+import { nanoid } from "nanoid";
 
 export default function Question({ question, allAnswers, qID, updateClicked }) {
-	//map through allAnswers array and get value,isClicked,isCorrect and id
-	const answerElements = allAnswers.map((answer) => {
-		return (
-			<button
-				key={nanoid()}
-				className="button--button"
-				onClick={() => updateClicked(qID, answer.id)}>
-				{he.decode(answer.value)}{' '}
-			</button>
-		);
-	});
+  //map through allAnswers array and get value,isClicked,isCorrect and id
+  const answerElements = allAnswers.map((answer) => {
+    const styles = {
+      backgroundColor: answer.isClicked ? "lightblue" : "transparent"
+    };
 
-	return (
-		<div className="questions">
-			<h3 className="questions--title">{he.decode(question)}</h3>
-			<div className="questions--button">{answerElements}</div>
-		</div>
-	);
+    return (
+      <button
+        key={nanoid()}
+        className="button--button"
+        onClick={(event) => updateClicked(qID, answer.id)}
+        style={styles}
+      >
+        {he.decode(answer.value)}{" "}
+      </button>
+    );
+  });
+
+  return (
+    <div className="questions">
+      <h3 className="questions--title">{he.decode(question)}</h3>
+      <div className="questions--button">{answerElements}</div>
+    </div>
+  );
 }
--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -1,85 +1,101 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
-import Question from './Question';
+import React from "react";
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+import Question from "./Question";
 
 export default function Quiz() {
-	const [quizData, setQuizData] = useState([]);
-	const [showAnswers, setShowAnswers] = useState(false);
-	const [resetQuiz, setResetQuiz] = useState(0);
+  const [quizData, setQuizData] = useState([]);
+  const [showAnswers, setShowAnswers] = useState(false);
+  const [resetQuiz, setResetQuiz] = useState(0);
 
-	console.log(quizData);
+  console.log(quizData);
 
-	useEffect(() => {
-		fetch(
-			'https://opentdb.com/api.php?amount=5&category=18&difficulty=medium&type=multiple'
-		)
-			.then((res) => res.json())
-			.then((data) =>
-				setQuizData(() => {
-					return data.results.map((question) => {
-						// 1 level map
+  useEffect(() => {
+    fetch(
+      "https://opentdb.com/api.php?amount=5&category=18&difficulty=medium&type=multiple"
+    )
+      .then((res) => res.json())
+      .then((data) =>
+        setQuizData(() => {
+          return data.results.map((question) => {
+            // 1 level map
 
-						const incorrectAnswer = question.incorrect_answers.map((item) => {
-							return {
-								value: item,
-								isClicked: false,
-								isCorrect: false,
-								id: nanoid()
-							};
-						}); // 2 level map
+            const incorrectAnswer = question.incorrect_answers.map((item) => {
+              return {
+                value: item,
+                isClicked: false,
+                isCorrect: false,
+                id: nanoid()
+              };
+            }); // 2 level map
 
-						const correctAnswer = {
-							value: question.correct_answer,
-							isClicked: false,
-							isCorrect: true,
-							id: nanoid()
-						};
+            const correctAnswer = {
+              value: question.correct_answer,
+              isClicked: false,
+              isCorrect: true,
+              id: nanoid()
+            };
 
-						const combinedAnswers = [...incorrectAnswer, correctAnswer]
-							.map((x) => [Math.random(), x])
-							.sort(([a], [b]) => a - b)
-							.map(([_, x]) => x);
+            const combinedAnswers = [...incorrectAnswer, correctAnswer]
+              .map((x) => [Math.random(), x])
+              .sort(([a], [b]) => a - b)
+              .map(([_, x]) => x);
 
-						return {
-							question: question.question,
-							answers: combinedAnswers,
-							id: nanoid()
-						};
-					}); // data.results.map
-				})
-			); //second then
-	}, [resetQuiz]); // end useEffect
+            return {
+              question: question.question,
+              answers: combinedAnswers,
+              id: nanoid()
+            };
+          }); // data.results.map
+        })
+      ); //second then
+  }, [resetQuiz]); // end useEffect
 
-	function updateClicked(qID, aID) {}
+  function updateClicked(qID, aID) {
+    setQuizData((oldData) => {
+      return oldData.map((question) => {
+        if (qID !== question.id) {
+          return question;
+        } else {
+          const newAnswers = question.answers.map((item) => {
+            return aID === item.id
+              ? { ...item, isClicked: true }
+              : { ...item, isClicked: false };
+          });
+          return { ...question, answers: newAnswers };
+        }
+      });
+    });
+  }
 
-	const questionElements = quizData.map((elements) => {
-		return (
-			<Question
-				key={nanoid()}
-				question={elements.question}
-				allAnswers={elements.answers}
-				updateClicked={updateClicked}
-				qID={elements.id}
-			/>
-		);
-	});
+  const questionElements = quizData.map((elements) => {
+    return (
+      <Question
+        key={nanoid()}
+        question={elements.question}
+        allAnswers={elements.answers}
+        updateClicked={updateClicked}
+        qID={elements.id}
+      />
+    );
+  });
 
-	return (
-		<div className="quiz">
-			{questionElements}
-			<div className="quiz--btn-chk">
-				<button
-					className="button--button"
-					style={{
-						marginTop: 30,
-						padding: 15,
-						backgroundColor: 'skyblue',
-						fontSize: 15
-					}}>
-					Check Answer
-				</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="quiz">
+      {questionElements}
+      <div className="quiz--btn-chk">
+        <button
+          className="button--button"
+          style={{
+            marginTop: 30,
+            padding: 15,
+            backgroundColor: "skyblue",
+            fontSize: 15
+          }}
+        >
+          Check Answer
+        </button>
+      </div>
+    </div>
+  );
 }
